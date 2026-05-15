134794aa0c62e432bfa3507f401b8c4e28bd033a
Paulo <pescanellasotero@gmail.com>
2026-05-15 09:59:53 +0200

    Inicializar clase Carta

--- src/main/java/Carta.java
+++ src/main/java/Carta.java
@@ -1,2 +1,32 @@
 public class Carta {
+    private String palo;
+    private int valor;
+    private boolean joker;
+
+    public Carta(String palo, int valor, boolean joker){
+        this.palo = palo;
+        this.valor = valor;
+        this.joker = joker;
+    }
+
+    public String getPalo(){
+        return palo;
+    }
+
+    public int getValor(){
+        return valor;
+    }
+
+    public boolean esJoker(){
+        return true;
+    }
+
+    public void setPalo(String palo) {
+        this.palo = palo;
+    }
+
+    public void setValor(int valor) {
+        this.valor = valor;
+    }
+
 }
