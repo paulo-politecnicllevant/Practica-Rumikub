cb848a8e67efecd9854d4261a8e80ea86a7d3ffd
Paulo <pescanellasotero@gmail.com>
2026-05-15 10:26:13 +0200

    Inicializo la clase Mazo y creo las cartas iniciales

--- src/main/java/Mazo.java
+++ src/main/java/Mazo.java
@@ -1,2 +1,24 @@
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class Mazo {
+    private ArrayList<Carta> cartas;
+
+    public Mazo(){
+        cartas = new ArrayList<>();
+        String[] palos = {"Corazon", "Diamante", "Pica", "Trebol"};
+
+        for (int i = 0; i < 2; i++){
+            for (String palo : palos){
+                for (int j = 0; j < 13; j++){
+                    cartas.add(new Carta(palo, j, false));
+                }
+            }
+        }
+        //Cartas joker
+        cartas.add(new Carta("", 0 , true));
+        cartas.add(new Carta("", 0 , true));
+
+        Collections.shuffle(cartas);
+    }
 }
