56273868ad4b4c8cba9c9baf608a25b30cb47631
Paulo <pescanellasotero@gmail.com>
2026-05-19 22:07:20 +0200

    Métodos para agregar y mostrar las jugadas válidas en el tablero

--- src/main/java/Mesa.java
+++ src/main/java/Mesa.java
@@ -1,2 +1,20 @@
+import java.util.ArrayList;
+
 public class Mesa {
+    private ArrayList<ArrayList<Carta>> jugadas;
+
+    public Mesa() {
+        jugadas = new ArrayList<>();
+    }
+
+    public void agregar(ArrayList<Carta> jugada) {
+        jugadas.add(jugada);
+    }
+
+    public void mostrar() {
+        System.out.println("============ MESA ============");
+        for (int i = 0; i < jugadas.size(); i++) {
+            System.out.println(i + ": " + jugadas.get(i));
+        }
+    }
 }
