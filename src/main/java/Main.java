b30ee66a0e90b74a6166fa97ca0c7c7203aa8d18
Paulo <pescanellasotero@gmail.com>
2026-05-19 22:12:50 +0200

    Inicialización de la partida desde la clase main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,2 +1,15 @@
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("=========== RUMMY CLÁSICO ===========");
+        System.out.print("Número de jugadores: ");
+
+        int n = sc.nextInt();
+
+        Juego juego = new Juego(n);
+        juego.iniciar();
+    }
 }
