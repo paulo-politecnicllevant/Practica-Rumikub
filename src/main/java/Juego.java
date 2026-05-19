ba1ff9d49ba0d8d9018a0ef86de41ae724bb4eac
Paulo <pescanellasotero@gmail.com>
2026-05-19 22:10:55 +0200

    Métodos para inicializar la parida y repartir las cartas entre los jugadores

--- src/main/java/Juego.java
+++ src/main/java/Juego.java
@@ -1,2 +1,30 @@
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Juego {
+    private ArrayList<Jugador> jugadores;
+    private Mazo mazo;
+    private Mesa mesa;
+    private Scanner scanner;
+
+    public Juego(int n) {
+        jugadores = new ArrayList<>();
+        mazo = new Mazo();
+        mesa = new Mesa();
+        scanner = new Scanner(System.in);
+
+        for (int i = 1; i <= n; i++) {
+            jugadores.add(new Jugador("Jugador " + i));
+        }
+
+        repartir();
+    }
+
+    private void repartir() {
+        for (int i = 0; i < 14; i++) {
+            for (Jugador j : jugadores) {
+                j.anyadirCarta(mazo.robar());
+            }
+        }
+    }
 }
