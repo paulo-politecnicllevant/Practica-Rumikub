e8d3bccfbe67666213f2e86f052925ae51f1561c
Paulo <pescanellasotero@gmail.com>
2026-05-19 21:57:39 +0200

    Método para comprobar que las cartas forman un gupo válido

--- src/main/java/Combinacion.java
+++ src/main/java/Combinacion.java
@@ -1,2 +1,23 @@
+import java.util.ArrayList;
+
 public class Combinacion {
+
+    public static boolean esGrupo(ArrayList<Carta> cartas) {
+        if (cartas.size() < 3 || cartas.size() > 4) return false;
+
+        int valor = -1;
+
+        for (Carta c : cartas) {
+            if (c.esJoker()){
+                continue;
+            }
+
+            if (valor == -1) {
+                valor = c.getValor();
+            }else if (valor != c.getValor()) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
