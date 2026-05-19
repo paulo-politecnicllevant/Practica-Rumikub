4bd5a215e29816a882d52c7ae1098bdb6049b8f1
Paulo <pescanellasotero@gmail.com>
2026-05-19 22:02:04 +0200

    Método para comprobar que las cartas forman una escalera válida y ordenarla

--- src/main/java/Combinacion.java
+++ src/main/java/Combinacion.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Combinacion {
 
@@ -8,16 +10,48 @@ public class Combinacion {
         int valor = -1;
 
         for (Carta c : cartas) {
-            if (c.esJoker()){
+            if (c.esJoker()) {
                 continue;
             }
 
             if (valor == -1) {
                 valor = c.getValor();
-            }else if (valor != c.getValor()) {
+            } else if (valor != c.getValor()) {
                 return false;
             }
         }
         return true;
+    }
+
+    public static boolean esEscalera(ArrayList<Carta> cartas) {
+        if (cartas.size() < 4) {
+            return false;
+        }
+
+        String palo = null;
+
+        for (Carta c : cartas) {
+            if (c.esJoker()) {
+                continue;
+            }
+
+            if (palo == null) {
+                palo = c.getPalo();
+            } else if (!palo.equals(c.getPalo())) {
+                return false;
+            }
+
+            Collections.sort(cartas, Comparator.comparingInt(Carta::getValor));
+
+            for (int i = 1; i < cartas.size(); i++) {
+                if (!cartas.get(i).esJoker() && !cartas.get(i - 1).esJoker()) {
+                    if (cartas.get(i).getValor() != cartas.get(i - 1).getValor() + 1)
+                        return false;
+                }
+            }
+
+            return true;
+        }
+        return false;
     }
 }
