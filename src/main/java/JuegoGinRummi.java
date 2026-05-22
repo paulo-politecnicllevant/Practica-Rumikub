6a2c30c7731b431a96e275e8fb88072abd96d04c
Paulo <pescanellasotero@gmail.com>
2026-05-22 11:08:40 +0200

    Creo el constructor de la clase y un método para repartir las cartas

--- src/main/java/JuegoGinRummi.java
+++ src/main/java/JuegoGinRummi.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+
 public class JuegoGinRummi extends JuegoBase{
+    private Mazo mazo;
+    private ArrayList<Carta> cartaDescarte;
+
     public JuegoGinRummi(int n) {
         super(n);
+        mazo = new Mazo();
+        cartaDescarte = new ArrayList<>();
+        repartir();
+    }
+
+    public void repartir(){
+        for (int i = 0; i < 10; i++){
+            for (Jugador j: jugadores){
+                j.anyadirCarta(mazo.robar());
+            }
+        }
     }
 
     @Override
