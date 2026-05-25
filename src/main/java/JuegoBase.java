9216a06cd5fa0a137e0bff984a21066919706710
Paulo <pescanellasotero@gmail.com>
2026-05-25 16:16:14 +0200

    Métodos en la clase juegoBase para guardar y cargar partida

--- src/main/java/JuegoBase.java
+++ src/main/java/JuegoBase.java
@@ -1,3 +1,4 @@
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -5,6 +6,9 @@ public abstract class JuegoBase {
     protected ArrayList<Jugador> jugadores;
     protected Mesa mesa;
     protected Scanner scanner;
+    protected Mazo mazo;
+    protected ArrayList<Carta> descarte;
+    protected int turnoActual;
 
     public JuegoBase(int n) {
         jugadores = new ArrayList<>();
@@ -17,4 +21,35 @@ public abstract class JuegoBase {
     }
 
     public abstract void iniciar();
+
+    public void guardarPartida(String archivo) {
+
+        try (ObjectOutputStream oos =
+                     new ObjectOutputStream(new FileOutputStream(archivo))) {
+
+            oos.writeObject(this);
+
+            System.out.println("Partida guardada correctamente");
+
+        } catch (IOException e) {
+            System.out.println("Error al guardar");
+            e.printStackTrace();
+        }
+    }
+
+    public static JuegoBase cargarPartida(String archivo) {
+
+        try (ObjectInputStream ois =
+                     new ObjectInputStream(new FileInputStream(archivo))) {
+
+            return (JuegoBase) ois.readObject();
+
+        } catch (Exception e) {
+
+            System.out.println("Error al cargar");
+            e.printStackTrace();
+
+            return null;
+        }
+    }
 }
