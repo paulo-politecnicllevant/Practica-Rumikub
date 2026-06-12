e7e1944a958609d65db38a6798ff7d47827953e5
Paulo <pescanellasotero@gmail.com>
2026-06-12 22:16:41 +0200

    Modifico la clase Mesa

--- src/main/java/Mesa.java
+++ src/main/java/Mesa.java
@@ -2,32 +2,32 @@ import java.io.Serializable;
 import java.util.ArrayList;
 
 public class Mesa implements Serializable {
-    private ArrayList<ArrayList<Carta>> jugadas;
+    private ArrayList<Combinacion> combinaciones;
 
     public Mesa() {
-        jugadas = new ArrayList<>();
+        combinaciones = new ArrayList<>();
     }
 
-    //Agrega una jugada a la mesa
-    public void agregar(ArrayList<Carta> jugada) {
-        if (Combinacion.esGrupo(jugada) || Combinacion.esEscalera(jugada)) {
-            jugadas.add(jugada);
+    //Agrega una combinacion a la mesa
+    public void agregar(Combinacion combinacion) {
+        if (combinacion.esValida()) {
+            combinaciones.add(combinacion);
         }else{
             System.out.println("COMBINACION INVALIDA");
         }
     }
 
-    //Muestra todas las jugadas que hay actualmente en la mesa
+    //Muestra todas las combinaciones que hay actualmente en la mesa
     public void mostrar() {
         System.out.println("============ MESA ============");
-        for (int i = 0; i < jugadas.size(); i++) {
-            System.out.println(i + ": " + jugadas.get(i));
+        for (int i = 0; i < combinaciones.size(); i++) {
+            System.out.println(i + ": " + combinaciones.get(i));
         }
     }
 
-    //Devuelve la lista completa de jugadas de la mesa
-    public ArrayList<ArrayList<Carta>> getJugadas() {
-        return jugadas;
+    //Devuelve la lista completa de combinaciones de la mesa
+    public ArrayList<Combinacion> getCombinaciones() {
+        return combinaciones;
     }
 
 }
