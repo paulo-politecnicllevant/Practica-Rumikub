234a6dd22887b537feedaa9b684fb8e508e4cfbe
Paulo <pescanellasotero@gmail.com>
2026-06-12 22:12:33 +0200

    Modifico la clase Combinacion

--- src/main/java/Combinacion.java
+++ src/main/java/Combinacion.java
@@ -1,3 +1,6 @@
+import enums.Palo;
+import enums.TipoJugada;
+
 import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Collections;
@@ -5,8 +8,37 @@ import java.util.Comparator;
 
 public class Combinacion implements Serializable {
 
+    private TipoJugada tipo;
+
+    private ArrayList<Carta> cartas;
+
+    public Combinacion(TipoJugada tipo){
+        this.tipo = tipo;
+        cartas = new ArrayList<>();
+    }
+
+    public TipoJugada getTipo() {
+        return tipo;
+    }
+
+    public ArrayList<Carta> getCartas() {
+        return cartas;
+    }
+
+    public void agregarCarta(Carta carta){
+        cartas.add(carta);
+    }
+
+    public boolean esValida(){
+        if (tipo == TipoJugada.GRUPO){
+            return esGrupo();
+        }else{
+            return esEscalera();
+        }
+    }
+
     //Compruebo si una lista de cartas forma un GRUPO
-    public static boolean esGrupo(ArrayList<Carta> cartas) {
+    public boolean esGrupo() {
         if (cartas.size() < 3 || cartas.size() > 4) return false;
 
         int valor = -1;
@@ -28,55 +60,60 @@ public class Combinacion implements Serializable {
     }
 
     // Compruebo si una lista de cartas forma un GRUPO
-    public static boolean esEscalera(ArrayList<Carta> cartas) {
-        if (cartas.size() < 4) {
+    private boolean esEscalera(){
+        if (cartas.size() < 4){
             return false;
         }
 
-        //Copia para no modificar la original
         ArrayList<Carta> copia = new ArrayList<>(cartas);
 
-        //Ordenar por valor
         copia.sort(Comparator.comparingInt(Carta::getValor));
 
-        String palo = null;
+        Palo palo = null;
 
-        //Comprobar que todas las cartas que no son comodín tienen el mismo palo
-        for (Carta c : copia) {
-            if (!c.esJoker()) {
-                if (palo == null) {
-                    palo = c.getPalo();
-                } else if (!palo.equals(c.getPalo())) {
-                    return false;
-                }
+        for (Carta c : copia){
+            if (c.esJoker()){
+                continue;
+            }
+
+            if (palo == null){
+                palo = c.getPalo();
+
+            } else if (palo != c.getPalo()){
+                return false;
             }
         }
 
-        //Contar comodines disponibles
         int jokers = (int) copia.stream().filter(Carta::esJoker).count();
 
-        // Comprobar si son consecutivas
-        for (int i = 1; i < copia.size(); i++) {
+        for (int i = 1; i < copia.size(); i++){
             Carta previa = copia.get(i - 1);
+
             Carta actual = copia.get(i);
 
-            if (previa.esJoker() || actual.esJoker()) continue;
-
-            //Si no son consecutivas
-            if (actual.getValor() != previa.getValor() + 1) {
-                //Mirar si puedo usar un comodín para rellenar el hueco
-                if (jokers > 0){
-                    jokers--;
-                } else{
-                    return false;
-                }
+            if (previa.esJoker() || actual.esJoker()){
+                continue;
             }
+
+            int diferencia = actual.getValor() - previa.getValor();
+
+            if (diferencia == 0){
+                return false;
+            }
+
+            int faltan = diferencia - 1;
+
+            if (faltan > jokers){
+                return false;
+            }
+            jokers -= faltan;
         }
+
         return true;
     }
 
     //Calcula los puntos totales de una combinación
-    public static int puntos(ArrayList<Carta> cartas) {
+    public int puntos() {
         int suma = 0;
 
         for (Carta c : cartas) {
